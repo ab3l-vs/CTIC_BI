d1f7f83b02674a958fd10978e81a09ba6d8a9917
abelvelasquez <abel.vel.sar@gmail.com>
2019-10-26 11:48:39 -0500

    Sexto comentario

--- ddl_tablas.sql
+++ ddl_tablas.sql
@@ -1,11 +1,13 @@
 -- creacion de tablas SQL
 -- autor : Abel Velasquez
 -- Trabajando en rama alterna
+-- Siguo en rama alterna
 
 create table td_cliente (
 nombre varchar2(50)
 apellidos varchar2(50)
 direccion varchar2(50)
 codpostal varchar2(50)
+comentarios varchar2(40)
 )
 tablespace tmp;
